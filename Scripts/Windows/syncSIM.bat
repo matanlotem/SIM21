e546755b5e753f212d5f18745bfcf5bf0450c104
matanlotem <lotemmtn@gmail.com>
2015-08-11 18:19:44 +0300

    sync script

---
+++ Scripts/Windows/syncSIM.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /command ^
+    "open sftp://matanlotem@power.tau.ac.il" ^
+    "keepuptodate -delete C:\Users\Matan\Work\SIM21 /a/home/cc/tree/taucc/students/physics/matanlotem/Work/SIM21" ^
+    exit
